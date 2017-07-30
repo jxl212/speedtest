1ae2247a9061bea25f45377651ade1cf858bd7e0
JXL <jeremy@mbp13.local>
2017-07-30 01:01:05 -0400

    Bit/s to MB/s
    changed the mongo db query

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ def index():
             "minutes" : {"$minute":"$_id"},
             "seconds": {"$second" : "$_id"}
           },
-          "download_min":{"$min": "$download"},
-          "download_max":{"$max": "$download"},
-          "download_avg":{"$avg":"$download"},
-          "upload_min":{"$min": "$download"},
-          "upload_max":{"$max": "$download"},
-          "upload_avg":{"$avg" : "$upload"},
+          "download_min":{"$multiply": [1.25e-7, {"$min": "$download"}]},
+          "download_max":{"$multiply": [1.25e-7, {"$max": "$download"}]},
+          "download_avg":{"$multiply": [1.25e-7, {"$avg":"$download"}]},
+          "upload_min":{"$multiply": [1.25e-7, {"$min": "$download"}]},
+          "upload_max":{"$multiply": [1.25e-7, {"$max": "$download"}]},
+          "upload_avg":{"$multiply": [1.25e-7, {"$avg" : "$upload"}]},
           "count":{"$sum":1}
           }
       },
